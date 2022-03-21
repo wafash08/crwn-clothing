38a4f08dde171ecf66d3b175cd8c7206eb7dc88a
wafash08 <saefulhaqwafa@gmail.com>
2022-03-21 23:31:09 +0700

    splitting out cart-item component into its own

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
 import "./categories.styles.scss";
+import categories from "./categories.json";
+import CategoryItem from "components/category-item/category-item.component";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Womens",
-    },
-    {
-      id: 5,
-      title: "Mens",
-    },
-  ];
-
   return (
     <div className='categories-container'>
       {categories.map(category => (
-        <div key={category.id} className='category-container'>
-          {/* <div
-            className='background-image'
-            style={{
-              backgroundImage: `url(${imageUrl})`,
-            }}
-          /> */}
-          <div className='category-body-container'>
-            <h2>{category.title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
