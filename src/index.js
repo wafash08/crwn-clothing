ee5fe26fdbf852a37ce9c6c8e7eefb078d4788c4
wafash08 <saefulhaqwafa@gmail.com>
2022-04-30 13:25:07 +0700

    creating product card component

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
